751cdd07b8118efb3697f9731b8ac36771a62601
Jacek Poleszak <j@pol.pm>
2023-04-25 12:28:43 +0100

    Added 2 challenges from Web Technologies (PHP)

---
+++ challenge1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Challenge 1</title>
+</head>
+
+<body>
+    <?php
+    $width = 10;
+    $height = 5;
+    $area = $width * $height;
+    echo "The rectangle has a width of " . $width . " meters, a height of " . $height . " meters, and and area of " . $area . " meters.";
+    ?>
+</body>
+
+</html>
---
+++ challenge2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Challenge 1</title>
+</head>
+
+<body>
+    <?php
+    $number1 = 10;
+    $number2 = 5;
+
+    // addition
+    $sum = $number1 + $number2;
+
+    // substraction
+    $difference = $number1 - $number2;
+
+    // multiplication
+    $product = $number1 + $number2;
+
+    // division
+    $quotient = $number1 / $number2;
+
+    // concatenation
+    $concatenation = $number1 . $number2;
+
+    echo "<p>Addition of ".$number1." and ".$number2." is ".$sum.".</p>";
+    echo "<p>Substraction of $number1 and $number2 is $difference.</p>";
+    echo "<p>Multiplication of $number1 and $number2 is $product.</p>";
+    echo "<p>Division of $number1 and $number2 is $quotient.</p>";
+    echo "<p>Concatenation of $number1 and $number2 is $concatenation.</p>";
+    ?>
+</body>
+
+</html>
