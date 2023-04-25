8ba7b239e1c829e7b4b7b193f94fb8d558d20f70
Jacek Poleszak <j@pol.pm>
2023-04-25 13:11:30 +0100

    Create challenge3.php

---
+++ challenge3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Challenge 3</title>
+</head>
+
+<body>
+    <?php
+    //date in mm/dd/yyyy format; or it can be in other formats as well
+    $birthDate = "11/14/1981";
+    //explode the date to get month, day and year
+    $birthDate = explode("/", $birthDate);
+    //get age from date or birthdate
+    $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+        ? ((date("Y") - $birthDate[2]) - 1)
+        : (date("Y") - $birthDate[2]));
+    echo "Age is: " . $age;
+    ?>
+</body>
+
+</html>
